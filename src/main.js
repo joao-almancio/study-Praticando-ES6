969e4a249fac6dbe4d0ec115202feeb00521a6b3
unknown <cacup7@gmail.com>
2020-04-24 17:39:12 -0300

    Integrada a visualiação dos dados da API

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ class App {
 
         this.formEl = document.getElementById('repo-form');
         this.inputEl = document.getElementById('repo-input');
+        this.listEl = document.getElementById('repo-list');
 
         this.eventHandler();
     }
@@ -25,14 +26,52 @@ class App {
         }
 
         try {
-               let data = await api.get(usuario);
-               this.userList.push(data);
+               let userData = await api.get(usuario);
+               let {avatar_url, name, location, html_url} = userData.data;
+
+               this.userList.push( {
+                   avatar_url,
+                   name,
+                   location,
+                   html_url,
+               });
 
                this.inputEl.value = '';
+
+               this.render();
         }
         catch (err) {
-            alert(`Usuario "${usuario}" inexistente. Tente otro nome.`);
+            console.log(`Usuario "${usuario}" inexistente. Tente otro nome.`);
         }
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.userList.forEach(item => {
+            let liEl = document.createElement('li');
+
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', item.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(item.name));
+
+            let textEl = document.createElement('p');
+            textEl.appendChild(document.createTextNode(item.location))
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', item.html_url);
+            linkEl.appendChild(document.createTextNode(item.html_url));
+
+            liEl.appendChild(imgEl);
+            liEl.appendChild(titleEl);
+            liEl.appendChild(textEl);
+            liEl.appendChild(linkEl);
+
+            this.listEl.appendChild(liEl);
+        })
     }
 }
 
